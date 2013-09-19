ab08d69594ef0a4286ff74c380a84b23dc43dd1c
tatabox2000 <tatabox2000@gmail.com>
2013-09-19 21:56:49 +0900

    cluster

---
+++ cluster.py
@@ -0,0 +1,37 @@
+import os
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage import io
+import scipy.ndimage as nd
+from matplotlib.ticker import FormatStrFormatter
+os.chdir(r'c://Users//analyst')
+
+def hist_cluster():
+	im = cv2.imread('440.bmp')
+	#im_HSV = cv2.cvtColor(im,cv2.COLOR_BGR2HSV)
+	h,edges =np.histogramdd(im.reshape(-1,3),8,range=[(0,255),(0,255),(0,255)])
+	h_flat=h.flatten()
+
+
+	plt.subplot(1,2,1),plt.plot(h_flat)
+
+	plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	hist_cluster()
+
