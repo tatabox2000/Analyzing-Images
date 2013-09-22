fc5a36cc291e6aea715184c675ced57636230d17
tatabox2000 <tatabox2000@gmail.com>
2013-09-22 12:09:52 +0900

    remote test

--- cluster.py
+++ cluster.py
@@ -2,13 +2,12 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-from skimage import io
 import scipy.ndimage as nd
 from matplotlib.ticker import FormatStrFormatter
 os.chdir(r'c://Users//analyst')
 
 def hist_cluster():
-	im = cv2.imread('440.bmp')
+	im = cv2.imread('00011.bmp')
 	#im_HSV = cv2.cvtColor(im,cv2.COLOR_BGR2HSV)
 	h,edges =np.histogramdd(im.reshape(-1,3),8,range=[(0,255),(0,255),(0,255)])
 	h_flat=h.flatten()
